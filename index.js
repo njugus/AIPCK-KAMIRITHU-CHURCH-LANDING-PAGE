e6a039ef60715c661a8c46effff53167b02c38f0
njugus <kayvohnjuguna@gmail.com>
2024-08-01 12:06:31 -0700

    adds the contact us an embed map

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 let slideIndex = 0;
 const slideShows = () => {
-    let slides = document.getElementsByClassName("slide");
-    //the for loop iterates over all the elemets in the slides array and sets their display property to none
-    for(i = 0;i < slides.length;i++){
-        slides[i].style.display = "none"
-    }
-    slideIndex++;
-    if(slideIndex >= slides.length) {slideIndex = 0}
-    slides[slideIndex].style.display = "block";
-    setTimeout(slideShows, 5000);
-}
+  let slides = document.getElementsByClassName("slide");
+  //the for loop iterates over all the elemets in the slides array and sets their display property to none
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  slides[slideIndex].style.display = "block";
+  setTimeout(slideShows, 5000);
+};
 slideShows();
